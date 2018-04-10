43b3cbda1edf6ab5f5561436d2bde968819829c0
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-04-10 16:45:57 -0500

    adding currentQuestion

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,42 @@
+$(document).ready(function () {
 
-var questions = [{
+var triviaQuestions = [{
     question: "Where was Liam born?",
-    answers: ["Hawaii", "Ireland", "Tokyo", "Germany"],
+    choices: ["Hawaii", "Ireland", "Tokyo", "Germany"],
     correctAnswer: 1
 }, {
     question: "Why did Liam start acting?",
-    answers: ["He always had a passion for it", "He was bullied and he thought being an actor would make him 'cooler'", "He wanted to impress his parents", "He wanted to imopress a girl at school"],
+    choices: ["He always had a passion for it", "He was bullied and he thought being an actor would make him 'cooler'", "He wanted to impress his parents", "He wanted to imopress a girl at school"],
     correctAnswer: 3
 }, {
     question: "When did Liam become a US citizen?",
-    answers: ["2009", "2005", "1999", "He's always been a US citizen"],
+    choices: ["2009", "2005", "1999", "He's always been a US citizen"],
     correctAnswer: 0
 }, {
     question: "True or false, Liam only agreed to be in the movie 'Taken' so he could learn karate?",
-    answers: ["True", "False"],
+    choices: ["True", "False"],
     correctAnswer: 0
 }, {
     question: "Liam was named after whom?",
-    answers: ["Liam Dunn, an American character actor", "His mother and fathers therapist", "The local Catholic priest", "His father"],
+    choices: ["Liam Dunn, an American character actor", "His mother and fathers therapist", "The local Catholic priest", "His father"],
     correctAnswer: 2
 }, {
     question: "True or false, Liam should have been chosen to play James Bond in Golden Eye instead of Pierce Brosnan?",
-    answers: ["True", "False"],
+    choices: ["True", "False"],
     correctAnswer: 0
 }];
 
 
-var correctAnswers = [];
+var userAnswers = [];
+var timer = "";
+var submit = "";
+var gameFinish = false;
 
+
+var currentQuestion = triviaQuestions[Math.floor(Math.random()*triviaQuestions.length)];
+
+console.log(currentQuestion);
+
+
+
+});
