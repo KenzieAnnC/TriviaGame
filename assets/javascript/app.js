ffbd3e4fab11e8b5f34f40ead128f7303674c8a5
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-04-13 12:54:12 -0500

    rewriting entire orderQuestions function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,88 +1,107 @@
 $(document).ready(function () {
 
-var triviaQuestions = [
-    {
-    question: "Where was Liam born?",
-    choices: ["Hawaii", "Ireland", "Tokyo", "Germany"],
-    correctAnswer: 1
-}, {
-    question: "Why did Liam start acting?",
-    choices: ["He always had a passion for it", "He was bullied and he thought being an actor would make him 'cooler'", "He wanted to impress his parents", "He wanted to impress a girl at school"],
-    correctAnswer: 3
-}, {
-    question: "When did Liam become a US citizen?",
-    choices: ["2009", "2005", "1999", "He's always been a US citizen"],
-    correctAnswer: 0
-}, {
-    question: "True or false, Liam only agreed to be in the movie 'Taken' so he could learn karate?",
-    choices: ["True", "False"],
-    correctAnswer: 0
-}, {
-    question: "Liam was named after whom?",
-    choices: ["Liam Dunn, an American character actor", "His mother and fathers therapist", "The local Catholic priest", "His father"],
-    correctAnswer: 2
-}, {
-    question: "True or false, Liam should have been chosen to play James Bond in Golden Eye instead of Pierce Brosnan?",
-    choices: ["True", "False"],
-    correctAnswer: 0
-}];
-
-
-var userAnswers = [];
-var timer = "";
-var submit = "";
-var questionsRight = 0;
-var questionsWrong = 0;
-var gameFinish = false;
-var nextQuestion = "";
-
-
-// get every question to display on the page with the possible answers directly underneath each question 
-
-function orderQuestions(){
-    triviaQuestions.forEach(function(element) {
-        $("#questions").append("<p>" + element.question + "</p>" + "<input type = radio>" + element.choices);
-
-    });
+    var triviaQuestions = [
+        {
+        question: "Where was Liam born?",
+        choices: ["Hawaii", "Ireland", "Tokyo", "Germany"],
+        correctAnswer: 1
+    }, {
+        question: "Why did Liam start acting?",
+        choices: ["He always had a passion for it", "He was bullied and he thought being an actor would make him 'cooler'", "He wanted to impress his parents", "He wanted to impress a girl at school"],
+        correctAnswer: 3
+    }, {
+        question: "When did Liam become a US citizen?",
+        choices: ["2009", "2005", "1999", "He's always been a US citizen"],
+        correctAnswer: 0
+    }, {
+        question: "True or false, Liam only agreed to be in the movie 'Taken' so he could learn karate?",
+        choices: ["True", "False"],
+        correctAnswer: 0
+    }, {
+        question: "Liam was named after whom?",
+        choices: ["Liam Dunn, an American character actor", "His mother and fathers therapist", "The local Catholic priest", "His father"],
+        correctAnswer: 2
+    }, {
+        question: "True or false, Liam should have been chosen to play James Bond in Golden Eye instead of Pierce Brosnan?",
+        choices: ["True", "False"],
+        correctAnswer: 0
+    }];
     
-    // for (i = 0; i < triviaQuestions.length; i++) {
-    //     // $("#quiz").append("<p>" + triviaQuestions.question);
-        
-    // }
-};
+    
+    var userGuess = [];
+    var timer = "";
+    var submit = "";
+    var questionsRight = 0;
+    var questionsWrong = 0;
+    var gameFinish = false;
+    var submit;
+    
+function resetGame() {
+    userGuess = [];
+    questionsRight = 0;
+    questionsWrong = 0;
+    gameFinish = false;
 
+}
 
 orderQuestions();
 
+function orderQuestions(){
 
-
-
-
-
-
-
-
-
-// var questions = triviaQuestions[Math.floor(Math.random()*triviaQuestions.length)];
-
-
-
-// console.log(currentQuestion);
-
-// function displayQuestions() {
-//     $("#questions").text(currentQuestion.question);
-//     currentQuestion.choices.forEach(function(element) {
-//         $("#choices").append("<li>" + "<input type = radio>" + element + "</li>");
-//       });
-// }
-
-// function makeGuess() {
+    triviaQuestions.forEach(function(currentQuestion) {
+        
+        // Start to create the HTML with a string...
+        var str ='';
+      
+        // We will put the question inside <p> tags to help with formating 
+        str += "<p>";
+      
+        // Current question
+        str += currentQuestion.question;
+      
+        str += "</p>";
+      
+        str += "<ul>";
+      
+        // currentQuestion.choices is an array of (string) choices
+        //
     
-// }
+        currentQuestion.choices.forEach(function(currentChoice, currentIndex) {
+          
+          
+          str += "<li>"
+          // currentQuestion.radioName is a property we can use to GROUP a bunch of radio buttons. All radio buttons in this group SHARE this. You will use this if you get to putting this into a form and submiting it.
+          // currentIndex will assign the index in the choices array (it looks like correctAnswer will use this, as it has the index of the correct choice)
+          str += '<input type="radio" name="'+currentQuestion.radioName+'" value="'+currentIndex+'">';
+          str += currentChoice;
+          str += "</li>"
+          
+          
+        });
+        str += "</ul>";
+        
+      
+        console.log(str);
+        
+        
+        // Select the div with the id of "questions"
+        $("#questions").append(str);
+
+    });
+    
+};
+
+
+function showGameResult() {
+    $("#questions").hide();
+    $("#quiz").text(questionsRight + "<br>" + questionsWrong);
+
+}
 
 
 
+    
 
+    
 
-
-});
+    });
